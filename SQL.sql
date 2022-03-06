0dd154e2701e508f8fa07ec27cbb00b9a7f67600
maxrio21 <49069348+maxrio21@users.noreply.github.com>
2022-03-06 19:11:21 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,56 @@
+--a.1
+SELECT *
+FROM CLIENTE
+WHERE correo LIKE '%hotmail%';
 
+--a.2
+SELECT *
+FROM ENTIDAD_BANCARIA
+WHERE codigo > '0300'
+ORDER BY (nombre);
+
+--a.3
+SELECT *
+FROM SUCURSAL
+WHERE 
+provincia NOT LIKE '%MADRID%' AND 
+ciudad = 'ALICANTE';
+
+--a.4
+SELECT *
+FROM CUENTA_BANCARIA
+WHERE 
+tipo = 'CORRIENTE' AND 
+id_cuenta BETWEEN 1 AND 5;
+
+--a.5
+SELECT *
+FROM CONTRATO
+WHERE clausulas NOT LIKE '';
+
+--b.1
+--b.2
+
+--c.1
+SELECT CONCAT(cl.nombre,' ',cl.apellidos) AS cliente,cu.iban,cu.deposito,cu.tipo,cu.contraseña
+FROM CLIENTE cl, CUENTA_BANCARIA cu
+WHERE 
+cl.id_cliente = cu.id_cliente
+ORDER BY deposito ASC;
+
+--c.2
+SELECT CONCAT(c.nombre,' ',c.apellidos) AS cliente,cb.iban,r.cantidad, r.fecha, r.tipo 
+FROM REALIZAR_OPERACION r, CLIENTE c, CUENTA_BANCARIA cb
+WHERE 
+r.id_cliente = c.id_cliente AND
+r.id_cuenta = cb.id_cuenta
+ORDER BY cantidad ASC
+
+SELECT *
+FROM FACTURA
+
+SELECT *
+FROM CUENTA_BANCARIA
+
+SELECT *
+FROM CLIENTE
